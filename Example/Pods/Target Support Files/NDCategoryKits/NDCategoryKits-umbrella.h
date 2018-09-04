d7bae9bcfa5dcbf6a86cab4e4696270953ab81b3
NewtonLi <dev_niudun@163.com>
2018-09-04 18:54:32 +0800

    add router

--- Example/Pods/Target Support Files/NDCategoryKits/NDCategoryKits-umbrella.h
+++ Example/Pods/Target Support Files/NDCategoryKits/NDCategoryKits-umbrella.h
@@ -13,6 +13,7 @@
 #import "FFCellProtocol.h"
 #import "FFMainView.h"
 #import "UITableViewCell+FFAdd.h"
+#import "CTMediator.h"
 
 FOUNDATION_EXPORT double NDCategoryKitsVersionNumber;
 FOUNDATION_EXPORT const unsigned char NDCategoryKitsVersionString[];
